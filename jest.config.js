e951e1f4de074723176cfe4741e22f887f472197
Martin Marosi <marvusm.mmi@gmail.com>
2020-10-20 16:16:19 +0200

    Collect coverage from tests.

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,6 @@
-module.exports = { preset: 'ts-jest', testEnvironment: 'jsdom', testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'] };
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(test).ts?(x)'],
+  collectCoverageFrom: ['<rootDir>/packages/**/src/**/*.ts', '<rootDir>/packages/**/src/**/*.tsx'],
+};
