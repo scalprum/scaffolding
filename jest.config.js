87f34a8aec772901a63603268a121aa77b9a3871
Martin Marosi <marvusm.mmi@gmail.com>
2020-10-23 12:18:17 +0200

    Add cypress script injection test.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(test).ts?(x)'],
-  collectCoverageFrom: ['<rootDir>/packages/**/src/**/*.ts', '<rootDir>/packages/**/src/**/*.tsx'],
+  collectCoverageFrom: [
+    '<rootDir>/packages/**/src/**/*.ts',
+    '<rootDir>/packages/**/src/**/*.tsx',
+    '!<rootDir>/packages/test-app/**/*.{ts,tsx,js,jsx}',
+  ],
   moduleDirectories: ['<rootDir>/node_modules', '<rootDir>/packages/**/node_modules'],
 };
