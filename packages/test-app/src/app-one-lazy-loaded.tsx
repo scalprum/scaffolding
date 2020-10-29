0eeaafc0212014e21149bf89787f27790eb1c493
Martin Marosi <marvusm.mmi@gmail.com>
2020-10-29 13:14:39 +0100

    Add nested route to verify advanced routing.

---
+++ packages/test-app/src/app-one-lazy-loaded.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const AppOneLazyLoaded = () => <h2>Lazy loaded route</h2>;
+
+export default AppOneLazyLoaded;
--- packages/test-app/src/appOne.tsx
+++ packages/test-app/src/appOne.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { render, unmountComponentAtNode } from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import { initializeApp } from '@scalprum/core';
 
-const AppOne = () => <h1>This is application one</h1>;
+const AppOneLazyLoaded = lazy(() => import('./app-one-lazy-loaded'));
+
+const AppOne = () => {
+  return (
+    <BrowserRouter basename="/app-one">
+      <ul>
+        <li>
+          <Link to="/">App one top</Link>
+        </li>
+        <li>
+          <Link to="/nested">App one nested route</Link>
+        </li>
+        <li>
+          <Link to="/nested-lazy">App one nested lazy route</Link>
+        </li>
+      </ul>
+      <div>
+        <Route path="/">
+          <h1>This is application one</h1>
+        </Route>
+        <Route exact path="/nested">
+          <h2>App one nested route</h2>
+        </Route>
+        <Route exact path="/nested-lazy">
+          <Suspense fallback={<div>Loading</div>}>
+            <AppOneLazyLoaded />
+          </Suspense>
+        </Route>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 initializeApp({
   id: 'app-one',
