c79405183628a037b37cd50550e40029d83ae178
Martin Marosi <marvusm.mmi@gmail.com>
2020-11-27 10:58:37 +0100

    Clean up ScalprumComponent.
    - allow customizing loading component
    - shield loading component from extra props
    - add additional catch branches to ScalprumComponent
    - props refactoring
    - test for various ScalprumComponent states
      - loading
      - suspesnse fallback
      - error state

--- packages/react-core/src/scalprum-component.tsx
+++ packages/react-core/src/scalprum-component.tsx
@@ -2,15 +2,18 @@ import React, { Fragment, useEffect, Suspense, useState } from 'react';
 import { getApp, getAppData, injectScript, processManifest } from '@scalprum/core';
 import { loadComponent } from './async-loader';
 
-export interface ScalprumComponentProps<T = Record<string, unknown>> {
-  fallback?: string;
+export type ScalprumComponentProps<API = Record<string, unknown>, Props = Record<string, unknown>> = Props & {
+  fallback?: NonNullable<React.ReactNode> | null;
   appName: string;
-  api?: T;
+  api?: API;
   scope: string;
   module: string;
   ErrorComponent?: React.ComponentType;
+  LoadingComponent?: React.ComponentType;
   processor?: (item: any) => string;
-}
+};
+
+const DefaultComponent: React.ComponentType = () => <Fragment />;
 
 export const ScalprumComponent: React.ComponentType<ScalprumComponentProps> = ({
   fallback = 'loading',
@@ -18,31 +21,40 @@ export const ScalprumComponent: React.ComponentType<ScalprumComponentProps> = ({
   api,
   scope,
   module,
-  ErrorComponent,
+  ErrorComponent = () => <span>Error while loading component!</span>,
   processor,
+  LoadingComponent = DefaultComponent,
   ...props
 }) => {
   const { scriptLocation, manifestLocation } = getAppData(appName);
-  const [Component, setComponent] = useState<React.ComponentType<any>>(Fragment);
+  const [Component, setComponent] = useState<React.ComponentType>(() => () => <LoadingComponent />);
   const [mountedAt, setMountedAt] = useState<HTMLScriptElement | HTMLScriptElement[] | undefined>();
   useEffect(() => {
     const app = getApp(appName);
 
     if (!app) {
       if (scriptLocation) {
-        injectScript(appName, scriptLocation).then(([, scriptMountedAt]) => {
-          const app = getApp(appName);
-          app?.mount<JSX.Element>(api);
-          setComponent(() => React.lazy(loadComponent(scope, module, ErrorComponent)));
-          setMountedAt(() => scriptMountedAt);
-        });
+        injectScript(appName, scriptLocation)
+          .then(([, scriptMountedAt]) => {
+            const app = getApp(appName);
+            app?.mount<JSX.Element>(api);
+            setComponent(() => React.lazy(loadComponent(scope, module, ErrorComponent)));
+            setMountedAt(() => scriptMountedAt);
+          })
+          .catch(() => {
+            setComponent(() => ErrorComponent);
+          });
       } else if (manifestLocation) {
-        processManifest(manifestLocation, appName, scope, processor).then((items) => {
-          setMountedAt(() => items.map((value) => (value as [unknown, HTMLScriptElement])[1]));
-          const app = getApp(appName);
-          app?.mount<JSX.Element>(api);
-          setComponent(() => React.lazy(loadComponent(scope, module, ErrorComponent)));
-        });
+        processManifest(manifestLocation, appName, scope, processor)
+          .then((items) => {
+            setMountedAt(() => items.map((value) => (value as [unknown, HTMLScriptElement])[1]));
+            const app = getApp(appName);
+            app?.mount<JSX.Element>(api);
+            setComponent(() => React.lazy(loadComponent(scope, module, ErrorComponent)));
+          })
+          .catch(() => {
+            setComponent(() => ErrorComponent);
+          });
       }
     } else {
       app?.mount<JSX.Element>(api);
