7c14fa2cc0fd5e1d3ef9caaf4da2daddc1c3cbfa
Karel Hala <khala@redhat.com>
2021-11-03 18:00:50 +0100

    Add use load module to add modules to DOM as well

--- packages/react-core/src/use-module.ts
+++ packages/react-core/src/use-module.ts
@@ -1,5 +1,15 @@
-import { useEffect, useState, useCallback } from 'react';
-import { asyncLoader, getCachedModule, IModule } from '@scalprum/core';
+import { useEffect, useState, useCallback, useRef } from 'react';
+import { asyncLoader, getCachedModule, IModule, getAppData, injectScript, processManifest } from '@scalprum/core';
+
+export type ModuleDefinition = {
+  appName?: string;
+  scope: string;
+  module: string;
+  processor?: (item: any) => string;
+};
+export interface IUseLoadModule {
+  moduleDef: ModuleDefinition;
+}
 
 export function useModule(
   scope: string,
@@ -36,4 +46,61 @@ export function useModule(
   }, [scope, module]);
 
   return data;
+}
+
+export function useLoadModule(
+  { appName, scope, module, processor }: ModuleDefinition,
+  defaultState: any,
+  options: {
+    skipCache?: boolean;
+  } = {}
+): [IModule | undefined, Error | undefined] {
+  const defaultOptions = {
+    skipCache: false,
+    ...options,
+  };
+  const { scriptLocation, manifestLocation } = getAppData(appName || scope);
+  const [data, setData] = useState<IModule>(defaultState);
+  const [error, setError] = useState<Error>();
+  const cachedModule = getCachedModule(scope, module, defaultOptions.skipCache);
+  const isMounted = useRef(true);
+  useEffect(() => {
+    if (isMounted.current) {
+      if (!cachedModule) {
+        if (scriptLocation) {
+          injectScript(appName || scope, scriptLocation)
+            .then(async () => {
+              const Module: IModule = await asyncLoader(scope, module);
+              setData(() => Module);
+            })
+            .catch((e) => {
+              setError(() => e);
+            });
+        } else if (manifestLocation) {
+          processManifest(manifestLocation, appName || scope, scope, processor)
+            .then(async () => {
+              const Module: IModule = await asyncLoader(scope, module);
+              setData(() => Module);
+            })
+            .catch((e) => {
+              setError(() => e);
+            });
+        }
+      } else {
+        try {
+          asyncLoader(scope, module).then((Module: IModule) => {
+            setData(() => Module);
+          });
+        } catch (e) {
+          setError(() => e as Error);
+        }
+      }
+    }
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, [appName, scope, cachedModule, defaultOptions.skipCache]);
+
+  return [data, error];
 }
